a0659a3ab00803676f32b5ac6f0fccbe5ae51f9a
Ivoox <ivoox45@gmail.com>
2025-06-11 19:46:01 -0500

    Structured project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                Hola Mundo!
+            </div>
+        </>
+    );
+}
+
+export default App;
